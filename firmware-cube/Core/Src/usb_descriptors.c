d19e751679f12663cc7cbce5890ba01b8a247767
heiso <alexandre.moatty@gmail.com>
2023-11-30 16:10:31 +0100

    hey it's working

--- firmware-cube/Core/Src/usb_descriptors.c
+++ firmware-cube/Core/Src/usb_descriptors.c
@@ -33,7 +33,7 @@
  *   [MSB]         HID | MSC | CDC          [LSB]
  */
 #define _PID_MAP(itf, n) ((CFG_TUD_##itf) << (n))
-#define USB_PID (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
+#define USB_PID (0x5000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
                  _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4))
 
 //--------------------------------------------------------------------+
@@ -49,7 +49,7 @@ tusb_desc_device_t const desc_device =
         .bDeviceProtocol = 0x00,
         .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
 
-        .idVendor = 0xCafe,
+        .idVendor = 0xCaff,
         .idProduct = USB_PID,
         .bcdDevice = 0x0100,
 
@@ -127,9 +127,9 @@ enum {
 char const *string_desc_arr[] =
     {
         (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-        "TinyUSB",                  // 1: Manufacturer
-        "TinyUSB Device",           // 2: Product
-        "123456",                   // 3: Serials will use unique ID if possible
+        "Heiso",                    // 1: Manufacturer
+        "Macrolev",                 // 2: Product
+        "234567",                   // 3: Serials will use unique ID if possible
 };
 
 static uint16_t _desc_str[32 + 1];
@@ -163,37 +163,37 @@ uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     }
   }
 
-  // switch (index) {
-  // case STRID_LANGID:
-  //   memcpy(&_desc_str[1], string_desc_arr[0], 2);
-  //   chr_count = 1;
-  //   break;
+  switch (index) {
+  case STRID_LANGID:
+    memcpy(&_desc_str[1], string_desc_arr[0], 2);
+    chr_count = 1;
+    break;
 
-  //   // case STRID_SERIAL:
-  //   //   chr_count = 10;
-  //   //   break;
+  case STRID_SERIAL:
+    chr_count = 10;
+    break;
 
-  // default:
-  //   // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-  //   // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
+  default:
+    // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
+    // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
 
-  //   if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
-  //     return NULL;
+    if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
+      return NULL;
 
-  //   const char *str = string_desc_arr[index];
+    const char *str = string_desc_arr[index];
 
-  //   // Cap at max char
-  //   chr_count = strlen(str);
-  //   size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1; // -1 for string type
-  //   if (chr_count > max_count)
-  //     chr_count = max_count;
+    // Cap at max char
+    chr_count = strlen(str);
+    size_t const max_count = sizeof(_desc_str) / sizeof(_desc_str[0]) - 1; // -1 for string type
+    if (chr_count > max_count)
+      chr_count = max_count;
 
-  //   // Convert ASCII string into UTF-16
-  //   for (size_t i = 0; i < chr_count; i++) {
-  //     _desc_str[1 + i] = str[i];
-  //   }
-  //   break;
-  // }
+    // Convert ASCII string into UTF-16
+    for (size_t i = 0; i < chr_count; i++) {
+      _desc_str[1 + i] = str[i];
+    }
+    break;
+  }
 
   // first byte is length (including header), second byte is string type
   _desc_str[0] = (uint16_t)((TUSB_DESC_STRING << 8) | (2 * chr_count + 2));
