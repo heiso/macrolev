efefa3bab2462761d312d589de7e196d6db86a10
heiso <alexandre.moatty@gmail.com>
2025-02-18 09:59:31 +0100

    wip: add some ui on config page

--- web-app/app/routes/config.tsx
+++ web-app/app/routes/config.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import { Link } from 'react-router'
+import { Field } from '../components/field'
 import { Keyboard } from '../components/keyboard/keyboard'
-import { initHistory, type KeyConfig, type State } from '../components/keyboard/keyboard-state'
-import { SwitchOffsetsConfiguration } from '../components/switch-offsets-configuration'
+import {
+  initHistory,
+  setKeycodeForSelectedKeys,
+  type KeyConfig,
+  type State,
+} from '../components/keyboard/keyboard-state'
 import defaultLayout from '../layouts/macrolev-azerty-mac-fr.json'
 
 export default function Index() {
@@ -10,6 +15,7 @@ export default function Index() {
     initHistory({
       keyConfigs: defaultLayout as KeyConfig[],
       selectedKeys: new Set<KeyConfig['id']>(),
+      selectedLayer: 0,
       history: [],
       currentHistoryIndex: 0,
       width: 0,
@@ -18,10 +24,39 @@ export default function Index() {
     }),
   )
 
+  // const [isListeningToKeydown, setIsListeningToKeydown] = useState(false)
+
+  // useEffect(() => {
+  //   if (isListeningToKeydown) {
+  //     window.addEventListener('keydown', handleKeydown)
+  //   } else {
+  //     window.removeEventListener('keydown', handleKeydown)
+  //   }
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeydown)
+  //   }
+  // }, [isListeningToKeydown])
+
+  // function handleKeydown(event: KeyboardEvent) {
+  //   if (state.selectedKeys.size > 0) {
+  //     setState(
+  //       setKeycodeForSelectedKeys(state, {
+  //         autoCapsVelocityTreshold: 0,
+  //         holdTimeBeforeActuation: 0,
+  //         isAutoCapsEnabled: 0,
+  //         keycodes: [{ type: 'NORMAL', value: parseInt(HID_KEYCODES[event.code], 16) }],
+  //       }),
+  //     )
+  //   }
+  //   setIsListeningToKeydown(false)
+  // }
+
   const layersCount = state.keyConfigs.reduce(
     (acc, keyConfig) => Math.max(acc, keyConfig.layers.length),
     0,
   )
+
+  const selectedKeys = state.keyConfigs.filter(({ id }) => state.selectedKeys.has(id))
 
   return (
     <div className="p-4 sm:p-8 flex flex-col items-center gap-8">
@@ -48,11 +83,31 @@ export default function Index() {
         </Link>
       </div>
 
-      <div className="flex gap-4 w-full">
-        <div className="flex flex-col gap-4">
+      <div className="flex w-full gap-4">
+        <div className="flex flex-col gap-2">
           {new Array(layersCount).fill(null).map((_, index) => (
-            <div key={index}>{index}</div>
+            <div
+              key={index}
+              className={`${state.selectedLayer === index ? 'bg-pink-500 text-pink-100' : 'bg-slate-500 text-slate-300'} text-center select-none active:scale-110 cursor-pointer px-4 py-2 rounded-sm sm:rounded-md text-nowrap`}
+              onClick={() => {
+                if (state.selectedLayer !== index) {
+                  setState((state) => ({ ...state, selectedLayer: index }))
+                }
+              }}
+            >
+              Layer {index}
+            </div>
           ))}
+          <div
+            className={`bg-slate-500 text-slate-300 text-center select-none active:scale-110 cursor-pointer px-4 py-2 rounded-sm sm:rounded-md text-nowrap`}
+            onClick={() => {
+              /**
+               * Add a new layer
+               */
+            }}
+          >
+            +
+          </div>
         </div>
 
         <Keyboard state={state} setState={setState} />
@@ -61,18 +116,62 @@ export default function Index() {
       {state.mode === 'configKeymap' && (
         <div className="flex gap-4 w-full">
           <div className="flex flex-col w-1/2">
-            <div
-              onKeyDown={(event) => console.log(event.code)}
-              className="flex justify-center items-center border border-slate-200 rounded-md w-full min-h-8"
-            >
-              {/* TODO: enable the keydown event only when user has clicked on a button, then disable the onkeydown when the keycode has been captured */}
-              <div>Type a key to set</div>
+            <div className="text-slate-400 text-xs uppercase">
+              <span
+                className="cursor-pointer hover:text-pink-400 active:text-pink-500"
+                onClick={() => navigator.clipboard.writeText(JSON.stringify(state.keyConfigs))}
+              >
+                Copy json to clipboard
+              </span>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              {/* <Button
+                disabled={state.selectedKeys.size === 0}
+                onClick={() => setIsListeningToKeydown(true)}
+              >
+                {isListeningToKeydown ? 'Listening...' : 'Click here to set a key'}
+              </Button> */}
+
+              <form className="flex flex-col gap-2">
+                <Field
+                  autoFocus
+                  key={`keycode-${selectedKeys.map(({ id }) => id).join('-')}`}
+                  label="Keycode"
+                  type="text"
+                  defaultValue={
+                    selectedKeys[0]?.layers[state.selectedLayer].keycodes[0].value !== undefined &&
+                    selectedKeys.every(
+                      ({ layers }) =>
+                        layers[state.selectedLayer].keycodes[0].value ===
+                        selectedKeys[0]?.layers[state.selectedLayer].keycodes[0].value,
+                    )
+                      ? `0x${selectedKeys[0]?.layers[state.selectedLayer].keycodes[0].value.toString(16).padStart(2, '0')}`
+                      : undefined
+                  }
+                  onChange={(event) => {
+                    const value = event.target.value === '' ? 0 : parseInt(event.target.value, 16)
+                    if (isNaN(value)) {
+                      return
+                    }
+
+                    setState((state) =>
+                      setKeycodeForSelectedKeys(state, {
+                        autoCapsVelocityTreshold: 0,
+                        holdTimeBeforeActuation: 0,
+                        isAutoCapsEnabled: 0,
+                        keycodes: [{ type: 'NORMAL', value }],
+                      }),
+                    )
+                  }}
+                  disabled={state.selectedKeys.size === 0}
+                  hint="Set a keycode for the selected keys in hexadecimal"
+                />
+              </form>
             </div>
           </div>
         </div>
       )}
-
-      <SwitchOffsetsConfiguration />
     </div>
   )
 }
